5b431cc1b2d047228247344ed2dcbc3e90ecdfa4
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2025-10-14 13:36:11 +0000

    resultado

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,14 @@
 // CRIE UMA LÓGICA AQUI
 
+let resultado  = 5
+do { 
+    console.log (`Resultado ${resultado}`)
+    resultado += 1 
+} while (resultado < 10 )
+console.log(`Resultado ${resultado}`)
+
+
+
+
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
