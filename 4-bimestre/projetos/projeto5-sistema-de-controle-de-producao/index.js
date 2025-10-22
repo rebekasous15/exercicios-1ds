336ce1d1a60121107a50c917c573300ef5c90b9e
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2025-10-22 00:38:54 +0000

    atualizado

--- 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
+++ 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
@@ -6,12 +6,12 @@ const prompt = require("prompt-sync")()
 // CRIE UMA LÓGICA ABAIXO
 
 let diaAtual = 1
-let continuar
+let continuar = ""
 let acumulado = 0
 let historico = ""
 
 do {
-  const metaTurno = parseInt(prompt("Quantas peças devem ser produzidas no turno? "))
+  let metaTurno = parseInt(prompt("Quantas peças devem ser produzidas no turno? "))
   let producao = 0
 
   for (let i = 1; i <= metaTurno; i++) {
@@ -22,14 +22,17 @@ do {
   acumulado += producao
   console.log(`Dia ${diaAtual}: ${producao} peças produzidas.`)
 
-  console.log(producao === metaTurno ? "Meta alcançada!" : "Meta não alcançada!")
-
-  historico += `Resumo dos dias anteriores:
-  Dia ${diaAtual}: ${producao} peças produzidas\n`
+  if(producao == metaTurno){
+    console.log("Meta alcançada!")
+  }else{
+    console.log("Meta não alcançada!")
+  }
+  historico += `Dia ${diaAtual}: ${producao} peças produzidas\n`
   diaAtual++
 
-  continuar = prompt("Deseja simular outro turno? (S/N) ").trim().toLowerCase()
+  continuar = prompt("Deseja simular outro turno? (S/N) ").toLowerCase()
 } while (continuar === "s")
+console.log("Resumo dos dias anteriores:")
 console.log(historico)
 console.log(`Total geral: ${acumulado}`)
 console.log("Encerrando sistema de produção...")
